fa16a5e8516bcfdfa7c5b50445e0ae2d80ce56fa
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-07-24 12:10:18 +0300

    Add endpoint get_movie_by_id

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -6,8 +6,31 @@ from sqlalchemy.orm import joinedload
 
 from database import get_db, MovieModel
 from database.models import CountryModel, GenreModel, ActorModel, LanguageModel
+from schemas import movies
 
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/{movie_id}/", response_model=movies.MovieDetailSchema)
+async def get_movie_by_id(
+    movie_id: int,
+    db: AsyncSession = Depends(get_db)
+) -> MovieModel:
+    result = await db.execute(
+        select(MovieModel)
+        .where(MovieModel.id == movie_id)
+        .options(joinedload(MovieModel.genres))
+        .options(joinedload(MovieModel.actors))
+        .options(joinedload(MovieModel.languages))
+        .options(joinedload(MovieModel.country))
+    )
+    movie_item: MovieModel = result.unique().scalar_one_or_none()
+
+    if not movie_item:
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with the given ID was not found."
+        )
+
+    return movie_item
