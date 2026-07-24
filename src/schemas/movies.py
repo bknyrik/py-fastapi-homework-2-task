328cc996c6d479dc2e6a98ffcdcc0015cac8363c
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-07-24 11:32:31 +0300

    Add schemas MovieListItemSchema

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,12 @@
-# Write your code here
+import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class MovieListItemSchema(BaseModel):
+    id: int
+    name: str
+    date: datetime.date
+    score: float
+    overview: str
+    model_config = ConfigDict(from_attributes=True)
